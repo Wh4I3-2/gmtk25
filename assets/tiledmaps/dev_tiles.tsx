4bb2f76ad61d9ec3b7b25886d5bba8cf1bd20730
Wh4I3-2 <154924333+Wh4I3-2@users.noreply.github.com>
2025-07-30 19:41:24 +0200

    dev tiledmap

---
+++ assets/tiledmaps/dev_tiles.tsx
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="dev_tiles" tilewidth="8" tileheight="8" tilecount="15" columns="3">
+ <image source="../textures/dev_tiles.png" width="24" height="40"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="4" y="4">
+    <polygon points="0,0 4,0 4,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="8" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="4" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="4" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="4" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="4" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="0" y="0" width="8" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="4" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4">
+    <polygon points="0,0 4,0 4,-4 8,-4 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="4">
+    <polygon points="0,0 0,4 -8,4 -8,-4 -4,-4 -4,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="4" height="4"/>
+   <object id="2" x="4" y="0" width="4" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="8">
+    <polygon points="0,0 4,0 4,-8 -4,-8 -4,-4 0,-4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="4">
+    <polygon points="0,0 -4,0 -4,4 -8,4 -8,-4 0,-4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="4" width="4" height="4"/>
+   <object id="2" x="0" y="0" width="4" height="4"/>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Dev Tiles" type="corner" tile="4">
+   <wangcolor name="" color="#ff0000" tile="4" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="6" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="7" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="8" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="10" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="11" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="12" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="13" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="14" wangid="0,0,0,1,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
